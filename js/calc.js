5a63dcfc01b800c17fb8aa5bc99f164c681a28e9
Michael Keyes <michael.d.keyes87@gmail.com>
2020-02-16 12:42:57 -0500

    Basic calculator functions

--- js/calc.js
+++ js/calc.js
@@ -1,13 +1,22 @@
 const calculator = document.querySelector('.calculator');
+const display = document.querySelector('.calculator-display');
 const keys = calculator.querySelector('.calculator-keys');
 
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    console.log(previousKeyType);
 
     if (!action) {
-      console.log('number key!');
+      if (displayedNum === '0' || previousKeyType === 'operator') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
     }
 
     if (
@@ -16,11 +25,14 @@ keys.addEventListener('click', e => {
       action === 'multiply' ||
       action === 'divide'
     ) {
-      console.log('operator key!');
+      key.classList.add('is-depressed');
+      calculator.dataset.previousKeyType = 'operator';
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
 
     if (action === 'decimal') {
-      console.log('decimal key!');
+      display.textContent = displayedNum + '.';
     }
 
     if (action === 'clear') {
@@ -28,7 +40,30 @@ keys.addEventListener('click', e => {
     }
 
     if (action === 'calculate') {
-      console.log('equal key!');
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
+
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
   }
 });
+
+const calculate = (n1, operator, n2) => {
+  // Perform calculation and return calculated value
+  let result = '';
+
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+
+  return result;
+}
